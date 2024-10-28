60c5b550618a48f67ad8da6d5bfb252911e7f45d
Ram kumar <4328468+ramthenmala@users.noreply.github.com>
2024-10-28 21:59:25 +0000

    fix: handle existing user attempt to register

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -2,9 +2,19 @@ import { Request, Response } from 'express';
 import logStatus from '../utils/logStatus';
 import { createUserService } from '../service/user.service';
 import { CreateUserInput } from '../types/ICreateUserInput';
+import UserModel from '../models/user.model';
 
 async function createUserController(req: Request<{}, {}, CreateUserInput>, res: Response) {
     try {
+        const existingUser = await UserModel.findOne({ email: req.body.email })
+
+        if (existingUser) {
+            logStatus.warn("User creation failed: user already exists with this email", req.body.email);
+            return res.status(400).json({
+                message: 'You have an account with us. Please try to login.'
+            });
+        }
+
         const user = await createUserService(req.body);
         
         const userWithoutPassword = {
